6d4d6d71f8187e9a633abd16999e30bac163e73d
patelashok <ashok.patel2917@gmail.com>
2021-06-12 17:36:48 +0530

    Take multiple Rides and calculate the aggregate total fare

---
+++ Ride.java
@@ -0,0 +1,11 @@
+package com.cabinvoice;
+public class Ride {
+
+    public int time;
+    public double distance;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}
